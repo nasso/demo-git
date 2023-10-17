24d3e902e79ac94c5646d7312b2c35330dd56900
nasso <nassomails@gmail.com>
2023-10-17 15:52:43 +0200

    Clear input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+  let newItemText = "";
+
+  function handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+
+    newItemText = "";
+  }
 </script>
 
-<form>
-  <input type="text" />
+<form on:submit={handleSubmit}>
+  <input type="text" bind:value={newItemText} />
   <button>Add</button>
 </form>
